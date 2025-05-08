88bfc8845d0e0544bf444de6c2f154af5f237e54
Selçuk TURAN - İŞ <selcuk.turann@outlook.com>
2025-05-09 02:17:59 +0300

    refactor(pb_hooks): remove unused middleware and clean up code refactor(docker-compose): comment out pb_hooks volume configuration

--- .slc-production/pocketbase/pb_hooks/main.pb.js
+++ .slc-production/pocketbase/pb_hooks/main.pb.js
@@ -1,27 +1,4 @@
-
-
-// attach global middleware
-routerUse((e) => {
-    console.log(1);
-    return e.next();
-});
-
-// attach global middleware with a custom priority
-routerUse(new Middleware((e) => {
-    console.log(2);
-    return e.next();
-}, -1));
-
-// attach middleware to a single route
-//
-// "GET /hello" should print the sequence: 2,1,3,4
-routerUse("GET", "/hello", (e) => {
-    console.log(4);
-    return e.string(200, "Hello! - production");
-}, (e) => {
-    console.log(3);
-    return e.next();
-});
+/// <reference path="../pb_data/types.d.ts" />
 
 onBootstrap((e) => {
     const utils = require(`${__hooks}/utils.js`);
@@ -29,13 +6,3 @@ onBootstrap((e) => {
     console.log("App initialized! - production");
     e.next();
 });
-
-onRecordsListRequest((e) => {
-    // e.app
-    // e.collection
-    // e.records
-    // e.result
-    // and all RequestEvent fields...
-    console.log("onRecordsListRequest - 1 - production");
-    e.next();
-}, "acl_roles_perms");
