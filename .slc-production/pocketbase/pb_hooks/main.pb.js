fc39c5d6f3b174cef7b09a9e0f93ccf3e19dc6ed
Selçuk TURAN - İŞ <selcuk.turann@outlook.com>
2025-05-08 04:28:26 +0300

    refactor: reorganize middleware setup and enhance logging in main.pb.js

--- .slc-production/pocketbase/pb_hooks/main.pb.js
+++ .slc-production/pocketbase/pb_hooks/main.pb.js
@@ -1,7 +1,41 @@
 /// <reference path="../../../.slc-local/pocketbase/pb_data/types.d.ts" />
 
-onAfterBootstrap((e) => {
-    const utils = require(`${__hooks}/utils.js`);
-
-    utils.hello("world");
+// attach global middleware
+routerUse((e) => {
+    console.log(1);
+    return e.next();
 });
+
+// attach global middleware with a custom priority
+routerUse(new Middleware((e) => {
+    console.log(2);
+    return e.next();
+}, -1));
+
+// attach middleware to a single route
+//
+// "GET /hello" should print the sequence: 2,1,3,4
+routerUse("GET", "/hello", (e) => {
+    console.log(4);
+    return e.string(200, "Hello! - production");
+}, (e) => {
+    console.log(3);
+    return e.next();
+});
+
+onBootstrap((e) => {
+    const utils = require(`${__hooks}/utils.js`);
+    utils.hello("world - production");
+    console.log("App initialized! - production");
+    e.next();
+});
+
+onRecordsListRequest((e) => {
+    // e.app
+    // e.collection
+    // e.records
+    // e.result
+    // and all RequestEvent fields...
+    console.log("onRecordsListRequest - 1 - production");
+    e.next();
+}, "acl_roles_perms");
