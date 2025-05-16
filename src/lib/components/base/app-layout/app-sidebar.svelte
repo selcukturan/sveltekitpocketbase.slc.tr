47d485e06c7782da99cc584cd57cfe6010d971a7
Selçuk TURAN - İŞ <selcuk.turann@outlook.com>
2025-05-17 00:01:04 +0300

    feat: Integrate new tooltip component from bits-ui; update dependencies and remove old tooltip implementation

--- src/lib/components/base/app-layout/app-sidebar.svelte
+++ src/lib/components/base/app-layout/app-sidebar.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import { getGlobalContext } from '$lib/client/app/global.svelte';
-	import tooltip from '$lib/client/actions/tooltip';
+	/* import tooltip from '$lib/client/actions/tooltip'; */
+	/* use:tooltip={{ text: d.title, position: global.isMobileBreakpoint ? 'bottom' : 'right' }} */
 	import type { PropsAppSidebarType } from './types';
 	import { Icon } from '$lib/components/icons';
+	import { Tooltip } from '$lib/components/zagjs/tooltip';
 
 	let { sidebarData }: PropsAppSidebarType = $props();
 
@@ -33,44 +35,48 @@
 			sm:overflow-y-auto"
 	>
 		{#each sidebarData as d, i}
-			<a
-				href={`${d.href}`}
-				aria-label={d.title}
-				aria-current={d.href === '/' && page.url.pathname === '/' ? 'page' : d.href !== '/' && page.url.pathname.startsWith(`${d.href}`) ? 'page' : undefined}
-				use:tooltip={{ text: d.title, position: global.isMobileBreakpoint ? 'bottom' : 'right' }}
-				class="
-					hover:bg-surface-200
-					active:bg-surface-300
-					aria-[current]:border-primary-900
-					aria-[current]:bg-primary-50
-					aria-[current]:text-primary-900
-					relative
-					inline-flex
-					min-h-[45px]
-					min-w-[45px]
-					cursor-pointer
-					items-center
-					justify-center
-					rounded-xl
-					border-2
-					border-solid
-					border-transparent
-					p-2
-					text-center
-					align-middle
-					text-2xl
-					leading-none
-					text-inherit
-					no-underline
-					outline-0
-					transition-colors
-					duration-150
-					select-none"
-			>
-				<span>
-					<Icon name={`${d.icon}`} size={global.isMobileBreakpoint ? `18px` : `22px`} />
-				</span>
-			</a>
+			<Tooltip contentProps={{ side: global.isMobileBreakpoint ? 'bottom' : 'right' }}>
+				{#snippet trigger()}
+					<a
+						href={`${d.href}`}
+						aria-label={d.title}
+						aria-current={d.href === '/' && page.url.pathname === '/' ? 'page' : d.href !== '/' && page.url.pathname.startsWith(`${d.href}`) ? 'page' : undefined}
+						class="
+						hover:bg-surface-200
+						active:bg-surface-300
+						aria-[current]:border-primary-900
+						aria-[current]:bg-primary-50
+						aria-[current]:text-primary-900
+						relative
+						inline-flex
+						min-h-[45px]
+						min-w-[45px]
+						cursor-pointer
+						items-center
+						justify-center
+						rounded-xl
+						border-2
+						border-solid
+						border-transparent
+						p-2
+						text-center
+						align-middle
+						text-2xl
+						leading-none
+						text-inherit
+						no-underline
+						outline-0
+						transition-colors
+						duration-150
+						select-none"
+					>
+						<span>
+							<Icon name={`${d.icon}`} size={global.isMobileBreakpoint ? `18px` : `22px`} />
+						</span>
+					</a>
+				{/snippet}
+				{d.title}
+			</Tooltip>
 		{/each}
 	</nav>
 	<footer class="flex items-center justify-center px-2 sm:py-2">
---
+++ src/lib/components/zagjs/tooltip/tooltip.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { Tooltip } from 'bits-ui';
+	import { type Snippet } from 'svelte';
+
+	type Props = Tooltip.RootProps & {
+		trigger: Snippet;
+		children: Snippet;
+		triggerProps?: Tooltip.TriggerProps;
+		contentProps?: Tooltip.ContentProps;
+	};
+
+	let { trigger, children, triggerProps = {}, contentProps = {}, open = $bindable(false), ...rootProps }: Props = $props();
+</script>
+
+<Tooltip.Root bind:open {...rootProps}>
+	<Tooltip.Trigger {...triggerProps}>
+		{@render trigger()}
+	</Tooltip.Trigger>
+	<Tooltip.Portal>
+		<Tooltip.Content {...contentProps}>
+			<Tooltip.Arrow />
+			{@render children()}
+		</Tooltip.Content>
+	</Tooltip.Portal>
+</Tooltip.Root>
