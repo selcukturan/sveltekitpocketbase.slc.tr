95c0013cb2b55407e484590a804c54bebe37f3c2
Selcuk TURAN <selcuk.turann@outlook.com>
2025-07-09 03:02:45 +0300

    feat: Update configuration and validation schemas - Bump version in config.ts to v0.1.0-alpha.89 - Add date validation schemas in valibot-schemas.ts, including ISO timestamp validation and conversion to Date object. - Implement text and number validation schemas with appropriate constraints. - Introduce new form components: ProxyNumberDecimal and ProxyNumberInteger for handling decimal and integer inputs. - Update existing form components to utilize new validation schemas and proxy components. - Refactor MyFirstForm.svelte to include new fields for integer and decimal numbers. - Enhance migration scripts to reflect new fields and validation rules in the database.

--- src/routes/(app)/developments/components/input-datetime/+page.svelte
+++ src/routes/(app)/developments/components/input-datetime/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import InputDateTime from '$lib/components/base/form/InputDateTime.svelte';
 	let value = $state(new Date());
 </script>
 
@@ -8,14 +7,6 @@
 		<h6>Tests Root Header</h6>
 	</div>
 	<div class="relative flex-1 overflow-x-hidden overflow-y-auto p-1">
-		<InputDateTime
-			bind:value
-			onchange={() => {
-				console.log('onchange');
-			}}
-			class="border-1 border-amber-600"
-		/>
-
 		<button
 			onclick={() => {
 				value = new Date();
