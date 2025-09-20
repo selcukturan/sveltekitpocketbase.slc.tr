fe18ab08232aa4d84457f3713ca519d1e55e2ab3
SLC Web Dev <selcuk.turann@outlook.com>
2025-09-20 16:31:02 +0300

    fix(config): update version to v0.1.0-alpha.205; refactor tableProps style definition in data table component

--- src/lib/components/base/data-table/tables.svelte.ts
+++ src/lib/components/base/data-table/tables.svelte.ts
@@ -1430,14 +1430,8 @@ class Table<TData extends Row> {
 			scroll-padding-block-end: ${this.srcFooters.length > 0 ? `${this.srcFooters.length * this.srcTfootRowHeight}px` : 'unset'};
 			scroll-padding-inline-start: ${this.focusedCellState?.colIndex === -1 || this.srcRowSelection === 'none' ? 'unset' : `${this.srcRowSelectionColumnWidth}px`};
 			scroll-padding-inline-end: ${this.focusedCellState?.colIndex === this.visibleColumns.length || this.srcRowAction === false ? 'unset' : `${this.srcRowActionColumnWidth}px`};
-			 */
-	tableProps = $derived({
-		role: 'grid',
-		class: 'slc-table',
-		tabindex: -1,
-		style: `
-			grid-template-rows: ${this.#gridTemplateRows};
-			grid-template-columns: ${this.#gridTemplateColumns};
+			 
+			
 			${
 				this.focusedCellState != null
 					? `scroll-padding-block-start: ${this.headerRowsCountState > 0 ? `${this.headerRowsCountState * this.srcTheadRowHeight}px` : 'unset'};
@@ -1446,6 +1440,15 @@ class Table<TData extends Row> {
 			scroll-padding-inline-end: ${this.focusedCellState?.colIndex === this.visibleColumns.length || this.srcRowAction === false ? 'unset' : `${this.srcRowActionColumnWidth}px`};`
 					: ''
 			}
+					*/
+	tableProps = $derived({
+		role: 'grid',
+		class: 'slc-table',
+		tabindex: -1,
+		style: `
+			grid-template-rows: ${this.#gridTemplateRows};
+			grid-template-columns: ${this.#gridTemplateColumns};
+			
 		`,
 		'aria-colcount': this.visibleColumns.length,
 		'aria-rowcount':
