08c7da53d78c9dddc5c117a1d4725d00c3d858e1
SLC Web Dev <selcuk.turann@outlook.com>
2025-12-30 14:35:10 +0300

    feat: increase file upload size limit in PocketBase and broaden accepted image types for file inputs.

---
+++ .slc-production/pocketbase/pb_migrations/1767094451_updated_test_datatable.js
@@ -0,0 +1,98 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3040204514")
+
+  // update field
+  collection.fields.addAt(1, new Field({
+    "hidden": false,
+    "id": "file1821200478",
+    "maxSelect": 1,
+    "maxSize": 10485760,
+    "mimeTypes": [
+      "image/jpeg",
+      "image/png",
+      "image/svg+xml",
+      "image/gif",
+      "image/webp"
+    ],
+    "name": "single_file",
+    "presentable": false,
+    "protected": true,
+    "required": false,
+    "system": false,
+    "thumbs": [],
+    "type": "file"
+  }))
+
+  // update field
+  collection.fields.addAt(2, new Field({
+    "hidden": false,
+    "id": "file2729472648",
+    "maxSelect": 99,
+    "maxSize": 10485760,
+    "mimeTypes": [
+      "image/jpeg",
+      "image/png",
+      "image/svg+xml",
+      "image/gif",
+      "image/webp"
+    ],
+    "name": "multiple_files",
+    "presentable": false,
+    "protected": true,
+    "required": false,
+    "system": false,
+    "thumbs": [],
+    "type": "file"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3040204514")
+
+  // update field
+  collection.fields.addAt(1, new Field({
+    "hidden": false,
+    "id": "file1821200478",
+    "maxSelect": 1,
+    "maxSize": 5242880,
+    "mimeTypes": [
+      "image/jpeg",
+      "image/png",
+      "image/svg+xml",
+      "image/gif",
+      "image/webp"
+    ],
+    "name": "single_file",
+    "presentable": false,
+    "protected": true,
+    "required": false,
+    "system": false,
+    "thumbs": [],
+    "type": "file"
+  }))
+
+  // update field
+  collection.fields.addAt(2, new Field({
+    "hidden": false,
+    "id": "file2729472648",
+    "maxSelect": 99,
+    "maxSize": 5242880,
+    "mimeTypes": [
+      "image/jpeg",
+      "image/png",
+      "image/svg+xml",
+      "image/gif",
+      "image/webp"
+    ],
+    "name": "multiple_files",
+    "presentable": false,
+    "protected": true,
+    "required": false,
+    "system": false,
+    "thumbs": [],
+    "type": "file"
+  }))
+
+  return app.save(collection)
+})
