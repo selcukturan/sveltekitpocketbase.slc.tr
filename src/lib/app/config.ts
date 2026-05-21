d05a27b996dd5a13fb5671e4cfe333bf5421afe7
SLC Web Dev <selcuk.turann@outlook.com>
2026-05-21 16:42:34 +0300

    chore: update version and configure VS Code extensions and settings for TODO tracking and workspace theming

--- src/lib/app/config.ts
+++ src/lib/app/config.ts
@@ -1,10 +1,33 @@
 // Server ve Client tarafında kullanılabilecek config dosyasıdır.
 // Bu dosya, uygulamanın genel ayarlarını ve yapılandırmalarını içerir.
 // kritik güvenlik ayarları burada tutulmaz.
+
 export const config = {
 	appName: 'SLC Web Applications',
-	version: 'v0.2.0-alpha.48',
+	version: 'v0.2.0-alpha.49',
 	copyright: `SLC Web Development © 2010-${new Date().getFullYear()}`
 } as const; // `as const` eklemek, objenin değerlerini readonly ve türlerini daha spesifik yapar.
 
 export type AppConfigType = typeof config;
+
+// TODO: yapılacak
+// HACK: hack
+// BUG: hata
+// MARK: işaretleme
+// FIXME: düzeltilmeli
+// XXX: önemli bir şeyler var
+// [ ] yapılacak
+// [/] yarı yapıldı
+// [x] yapıldı
+
+/**
+ * TODO: yapılacak
+ * HACK: hack
+ * BUG: hata
+ * MARK: işaretleme
+ * FIXME: düzeltilmeli
+ * XXX: önemli bir şeyler var
+ * [ ] yapılacak
+ * [/] yarı yapıldı
+ * [x] yapıldı
+ */
