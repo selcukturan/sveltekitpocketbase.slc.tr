9308b7672c524011800b02f806097e8182865d10
SLC Web Dev <selcuk.turann@outlook.com>
2026-06-08 12:16:50 +0300

    chore: update app version to v0.2.0-alpha.60 and set service dependency condition to healthy in docker-compose

--- src/lib/app/config.ts
+++ src/lib/app/config.ts
@@ -4,7 +4,7 @@
 
 export const config = {
 	appName: 'SLC Web Applications',
-	version: 'v0.2.0-alpha.59',
+	version: 'v0.2.0-alpha.60',
 	copyright: `SLC Web Development © 2010-${new Date().getFullYear()}`
 } as const; // `as const` eklemek, objenin değerlerini readonly ve türlerini daha spesifik yapar.
 
