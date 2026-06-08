e1e8b30e7803dd0d466a187367ddffc508ba5a24
SLC Web Dev <selcuk.turann@outlook.com>
2026-06-08 15:53:43 +0300

    chore: bump application version to v0.2.0-alpha.63

--- src/lib/app/config.ts
+++ src/lib/app/config.ts
@@ -4,7 +4,7 @@
 
 export const config = {
 	appName: 'SLC Web Applications',
-	version: 'v0.2.0-alpha.62',
+	version: 'v0.2.0-alpha.63',
 	copyright: `SLC Web Development © 2010-${new Date().getFullYear()}`
 } as const; // `as const` eklemek, objenin değerlerini readonly ve türlerini daha spesifik yapar.
 
