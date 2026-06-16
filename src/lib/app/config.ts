59ef2bb6b847b387fd1f432d07ebdb521d97d4f7
SLC Web Dev <selcuk.turann@outlook.com>
2026-06-16 22:48:18 +0300

    chore: remove service healthchecks and increment application version to v0.2.0-alpha.73

--- src/lib/app/config.ts
+++ src/lib/app/config.ts
@@ -4,7 +4,7 @@
 
 export const config = {
 	appName: 'SLC Web Applications',
-	version: 'v0.2.0-alpha.72',
+	version: 'v0.2.0-alpha.73',
 	copyright: `SLC Web Development © 2010-${new Date().getFullYear()}`
 } as const; // `as const` eklemek, objenin değerlerini readonly ve türlerini daha spesifik yapar.
 
