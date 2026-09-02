049ed8a4500945f092dffff856c36dd383a19c80
Selçuk Turan <slcwebtr@gmail.com>
2026-09-02 16:25:03 +0300

    chore: bump version to alpha.102

--- src/lib/app/config.ts
+++ src/lib/app/config.ts
@@ -4,7 +4,7 @@
 
 export const config = {
 	appName: 'SLC Web Applications',
-	version: 'v0.2.0-alpha.100',
+	version: 'v0.2.0-alpha.102',
 	copyright: `SLC Web Development © 2010-${new Date().getFullYear()}`
 } as const; // `as const` eklemek, objenin değerlerini readonly ve türlerini daha spesifik yapar.
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { pocketbaseTypegenPlugin } from './.slc-development/my-vite-plugin';
+import { pocketbaseTypegenPlugin } from './.slc-development/my-vite-plugin/index.ts';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit(), pocketbaseTypegenPlugin()],
 	server: {
 		watch: {
-			ignored: [
-				'**/.slc-development/**',
-				'**/.slc-local/**',
-				'**/.slc-production/pocketbase/pb_data/**'
-			]
+			ignored: ['**/.slc-development/**', '**/.slc-local/**', '**/.slc-production/pocketbase/pb_data/**']
 		}
 	}
 });
